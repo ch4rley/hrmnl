43cae8e0c6b800afb607beee5d5bec33d22eabca
ch4rley <charleydactyl@gmail.com>
2022-05-03 23:48:33 -0400

    added a little to the site controller

--- controllers/siteCtrl.js
+++ controllers/siteCtrl.js
@@ -2,11 +2,17 @@ const siteData = require('../data/siteData');
 //const SchemaName = require('../models/schemanameModel');
 
 module.exports = {
-  index: (request, response) => {
+    // handler that will show index page
+    index: (request, response) => {
     response.render('pages/index', {
         name: siteData.userName,
         copyrightYear: siteData.year,
         signedIn: siteData.signedIn
-    });
-  }
+        });
+    },
+    // handler that will show login page
+    login: (request, response) => {
+        response.render('pages/login');
+    }, 
+
 }
