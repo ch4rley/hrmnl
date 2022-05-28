7d345eae6ee0f6e77491bde7b4088b358e1eb997
charley lightstone <talk.to.ch4rley@gmail.com>
2022-05-28 12:06:47 -0400

    converted html files to ejs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// import express
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const methodOverride = require('method-override');
 const routes = require('./routes/index');
+// create new express app and save it as 'app'
 const app = express();
+// server configuration
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
@@ -15,8 +18,9 @@ app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(routes);
 
-require('./config/connection');
+// require('./config/connection');
 
+// make server listen to requests on specified PORT and give feedback
 app.listen(PORT, () => {
     console.log(`the server is listening, respectfully, on port ${PORT}`);
 });
