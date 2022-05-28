9f5f7c67c347d59ef20370a60a2657e18c8b171a
charley lightstone <talk.to.ch4rley@gmail.com>
2022-05-28 19:27:55 -0400

    Changed edit-profile page header to say update profile,  added reminder note to add method override, added ejs for checking the email box if user has already opted-in to emails, added height of 100vh to body and overflow: scroll, created partials for hormone-specific forms, and changes my if statements in profile-edit.ejs to account for hormone key to hold and array-type value in the user data.

--- data/userData.js
+++ data/userData.js
@@ -2,12 +2,12 @@ module.exports = {
     user: {
     name: 'charley',
     pronouns: 'they/them/theirs',
-    hormone: 'testosterone',
+    hormone: ['testosterone', 'estrogen'],
     hrtDeliveryT: 'subcutaneous injection',
     hrtDelivery: 'subcutaneous injection',
-    hrtDose: '.09mL',
-    hrtConcentration: '200mg/mL',
-    hrtFrequency: '1x/week',
-    email_newFeatures: false,
+    hrtDoseT: '.09mL',
+    hrtConcentrationT: '200mg/mL',
+    hrtFrequencyT: '1x/week',
+    email_newFeatures: true,
   }
 }
