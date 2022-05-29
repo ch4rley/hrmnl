46a661f3f2613a334fd557f8a0adaa0421337789
charley lightstone <talk.to.ch4rley@gmail.com>
2022-05-29 15:54:03 -0400

    Created event listeners for checkboxes on create-profile page that will show the appropriate fields when user selects hormone

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -15,6 +15,8 @@
 //   };
 // };
 
+
+// DISPLAY APPROPRIATE HRT INPUT FIELDS
 let detailsE = document.querySelector('#hrtDetailsE');
 let detailsT = document.querySelector('#hrtDetailsT');
 let detailsP = document.querySelector('#hrtDetailsP');
@@ -24,13 +26,46 @@ let E = document.querySelector('#checkboxE');
 let T = document.querySelector('#testosterone');
 let P = document.querySelector('#progesterone');
 
+// function will hide hrt details by default when other details show
+const hideDefaultHrtDetails = () => {
+  if(details.classList.contains('hidden') === false) {
+    details.classList.toggle('hidden');
+  } else {
+    if(
+      detailsE.classList.contains('open') === false
+      &&
+      detailsT.classList.contains('open') === false
+      &&
+      detailsP.classList.contains('open') === false) {
+        details.classList.toggle('hidden');
+      };
+  }
+};
+
+// show HRT details input fields for estrogen, if user clicks on estrogen, and hide basic details input fields, if they are not already hidden
 const showHrtDetailsE = function(e) {
   detailsE.classList.toggle('open');
-  details.classList.toggle('hidden');
+  hideDefaultHrtDetails();
+  console.log(e);
+};
 
+// show HRT details input fields for testosterone, if user clicks on testosterone, and hide basic details input fields, if they are not already hidden
+const showHrtDetailsT = function(e) {
+  detailsT.classList.toggle('open');
+  hideDefaultHrtDetails();
+  console.log(e);
+};
+
+T.addEventListener('click', showHrtDetailsT);
+
+const showHrtDetailsP = function(e) {
+  detailsP.classList.toggle('open');
+  hideDefaultHrtDetails();
   console.log(e);
 };
 
 E.addEventListener('click', showHrtDetailsE);
+T.addEventListener('click', showHrtDetailsT);
+P.addEventListener('click', showHrtDetailsP);
 
 
