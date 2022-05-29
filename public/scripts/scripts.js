51f3e8c2bc8101fddd4cfa8760b3899edc41e3c3
charley lightstone <talk.to.ch4rley@gmail.com>
2022-05-29 15:26:42 -0400

    Added .open to CSS to display flex/column, wrote a click event for showing correct fields for art Details based on user clicking specific checkbox, and toggled hidden for default options checkbox

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,16 +1,36 @@
 // MODAL BUTTON
 
-let modalBtn = document.getElementById("modal-btn")
-let modal = document.querySelector(".modal")
-let closeBtn = document.querySelector(".close-btn")
-modalBtn.onclick = function(){
-  modal.style.display = "block"
-}
-closeBtn.onclick = function(){
-  modal.style.display = "none"
-}
-window.onclick = function(e){
-  if(e.target == modal){
-    modal.style.display = "none"
-  }
-}
+// let modalBtn = document.getElementById('modal-btn');
+// let modal = document.querySelector('.modal');
+// let closeBtn = document.querySelector('.close-btn');
+// modalBtn.onclick = function(){
+//   modal.style.display = 'block';
+// };
+// closeBtn.onclick = function(){
+//   modal.style.display = 'none';
+// };
+// window.onclick = function(e){
+//   if(e.target == modal){
+//     modal.style.display = 'none';
+//   };
+// };
+
+let detailsE = document.querySelector('#hrtDetailsE');
+let detailsT = document.querySelector('#hrtDetailsT');
+let detailsP = document.querySelector('#hrtDetailsP');
+let details = document.querySelector('#hrtDetails');
+
+let E = document.querySelector('#checkboxE');
+let T = document.querySelector('#testosterone');
+let P = document.querySelector('#progesterone');
+
+const showHrtDetailsE = function(e) {
+  detailsE.classList.toggle('open');
+  details.classList.toggle('hidden');
+
+  console.log(e);
+};
+
+E.addEventListener('click', showHrtDetailsE);
+
+
