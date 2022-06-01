fcc119c7cb952252b7d3a6e7d9069f423cb46e6e
charley lightstone <talk.to.ch4rley@gmail.com>
2022-06-01 10:38:57 -0400

    working out issues with login modal and scripts

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,69 +1,84 @@
 // MODAL BUTTON
 
-// let modalBtn = document.getElementById('modal-btn');
-// let modal = document.querySelector('.modal');
-// let closeBtn = document.querySelector('.close-btn');
-// modalBtn.onclick = function(){
-//   modal.style.display = 'block';
+let modalBtnSignup = document.getElementById('modal-btn-signup');
+let modalSignup = document.querySelector('.modal-signup');
+let closeBtn = document.querySelector('.close-btn');
+modalBtnSignup.onclick = function(){
+  modalSignup.style.display = 'block';
+};
+closeBtn.onclick = function(){
+  modalSignup.style.display = 'none';
+};
+window.onclick = function(e){
+  if(e.target == modalSignup){
+    modalSignup.style.display = 'none';
+  };
+};
+
+let modalBtnLogin = document.getElementById('modal-btn-login');
+let modalLogin = document.querySelector('.modal-login');
+let closeBtn = document.querySelector('.close-btn');
+modalBtnLogin.onclick = function(){
+  modalLogin.style.display = 'block';
+};
+closeBtn.onclick = function(){
+  modalLogin.style.display = 'none';
+};
+window.onclick = function(e){
+  if(e.target == modalLogin){
+    modalLogin.style.display = 'none';
+  };
+};
+
+
+// // DISPLAY APPROPRIATE HRT INPUT FIELDS
+// let detailsE = document.querySelector('#hrtDetailsE');
+// let detailsT = document.querySelector('#hrtDetailsT');
+// let detailsP = document.querySelector('#hrtDetailsP');
+// let details = document.querySelector('#hrtDetails');
+
+// let E = document.querySelector('#checkboxE');
+// let T = document.querySelector('#checkboxT');
+// let P = document.querySelector('#checkboxP');
+
+// // function will hide hrt details by default when other details show
+// const hideDefaultHrtDetails = () => {
+//   if(details.classList.contains('hidden') === false) {
+//     details.classList.toggle('hidden');
+//   } else {
+//     if(
+//       detailsE.classList.contains('open') === false
+//       &&
+//       detailsT.classList.contains('open') === false
+//       &&
+//       detailsP.classList.contains('open') === false) {
+//         details.classList.toggle('hidden');
+//       };
+//   }
 // };
-// closeBtn.onclick = function(){
-//   modal.style.display = 'none';
+
+// // show HRT details input fields for estrogen, if user clicks on estrogen, and hide basic details input fields, if they are not already hidden
+// const showHrtDetailsE = function(e) {
+//   detailsE.classList.toggle('open');
+//   hideDefaultHrtDetails();
+//   console.log(e);
 // };
-// window.onclick = function(e){
-//   if(e.target == modal){
-//     modal.style.display = 'none';
-//   };
+
+// // show HRT details input fields for testosterone, if user clicks on testosterone, and hide basic details input fields, if they are not already hidden
+// const showHrtDetailsT = function(e) {
+//   detailsT.classList.toggle('open');
+//   hideDefaultHrtDetails();
+//   console.log(e);
 // };
 
+// const showHrtDetailsP = function(e) {
+//   detailsP.classList.toggle('open');
+//   hideDefaultHrtDetails();
+//   console.log(e);
+// };
 
-// DISPLAY APPROPRIATE HRT INPUT FIELDS
-let detailsE = document.querySelector('#hrtDetailsE');
-let detailsT = document.querySelector('#hrtDetailsT');
-let detailsP = document.querySelector('#hrtDetailsP');
-let details = document.querySelector('#hrtDetails');
-
-let E = document.querySelector('#checkboxE');
-let T = document.querySelector('#checkboxT');
-let P = document.querySelector('#checkboxP');
-
-// function will hide hrt details by default when other details show
-const hideDefaultHrtDetails = () => {
-  if(details.classList.contains('hidden') === false) {
-    details.classList.toggle('hidden');
-  } else {
-    if(
-      detailsE.classList.contains('open') === false
-      &&
-      detailsT.classList.contains('open') === false
-      &&
-      detailsP.classList.contains('open') === false) {
-        details.classList.toggle('hidden');
-      };
-  }
-};
-
-// show HRT details input fields for estrogen, if user clicks on estrogen, and hide basic details input fields, if they are not already hidden
-const showHrtDetailsE = function(e) {
-  detailsE.classList.toggle('open');
-  hideDefaultHrtDetails();
-  console.log(e);
-};
-
-// show HRT details input fields for testosterone, if user clicks on testosterone, and hide basic details input fields, if they are not already hidden
-const showHrtDetailsT = function(e) {
-  detailsT.classList.toggle('open');
-  hideDefaultHrtDetails();
-  console.log(e);
-};
-
-const showHrtDetailsP = function(e) {
-  detailsP.classList.toggle('open');
-  hideDefaultHrtDetails();
-  console.log(e);
-};
-
-E.addEventListener('click', showHrtDetailsE);
-T.addEventListener('click', showHrtDetailsT);
-P.addEventListener('click', showHrtDetailsP);
+// E.addEventListener('click', showHrtDetailsE);
+// T.addEventListener('click', showHrtDetailsT);
+// P.addEventListener('click', showHrtDetailsP);
 
 
