b9ace92f79c46bde966ee7a886b7592d81f7fcf0
charley lightstone <talk.to.ch4rley@gmail.com>
2022-06-05 14:48:48 -0400

    installed these packages: express-session mongoose-findorcreate passport passport-google-oauth20 passport-local passport-local-mongoose dotenv, added oauth info to dotenv, required dotenv in config/connection, importedimported espress session and passport to app.js, added session and passport as middleware, and initialized passport, allowed passport to utilize session

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const morgan = require('morgan');
 const path = require('path');
 // import method override to allow update and delete methods on forms
 const methodOverride = require('method-override');
+// import express session
+const session = require('express-session');
+// import passport
+const passport = require('passport');
 // point to routes in index.js file
 const routes = require('./routes/index');
 // create new express app and save it as 'app'
@@ -18,7 +22,7 @@ const PORT = 3000;
 // configure node.js to use ejs as views engine
 app.set('view engine', 'ejs');
 
-// use path module to point Node.js to public directory
+// use path module to point Node.js to public directory for images, styles, scripts
 app.use(express.static(path.join(__dirname, 'public')));
 // parse content encoded in the url
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +31,18 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 // use morgan as middleware
 app.use(morgan('dev'));
+
+// tell app to utilize session (express-session)
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false
+  }));
+
+// initialize passport
+app.use(passport.initialize());
+// allows passport to utilize session
+app.use(passport.session());
 
 // tells app that routes exist and where to find them
 app.use(routes);
--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,5 @@
+// connects secret information (environment variables)
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
